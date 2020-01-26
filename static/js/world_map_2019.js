d91bc7e808d877836291fd69e7caff29fc38749b
Lolita Dias <lolita.dias@gmail.com>
2020-01-26 14:52:13 -0500

    Updated app.py to read 2019,2018,2017,2016 and 2015 data from mongodb

--- static/js/world_map_2019.js
+++ static/js/world_map_2019.js
@@ -1,5 +1,5 @@
 
-d3.json("http://localhost:5000/project2/worldmap_data").then(function(data){
+d3.json("http://localhost:5000/worldmap_data").then(function(data){
     // console.log(data);
     country_data=[];
     score_data=[];
