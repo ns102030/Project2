c9c3833c0c02e5b89af8448f1ce47444aa45a602
Lolita Dias <lolita.dias@gmail.com>
2020-01-29 14:16:30 -0500

    Added code for country filters

---
+++ static/js/dropdown.js
@@ -0,0 +1,93 @@
+//Define worldmap function
+function build_world_map(year_val) {
+    console.log("I am in worldmap function_1")
+	d3.json("http://localhost:5000/worldmap_data").then ( data =>{
+	  world_map(data,year_val);
+	})
+};
+
+//Define and choose data to build summary table
+function build_summary_table(year_val,country_val){
+    console.log("I am in the define-function for table");
+    if(year_val==2015){
+        d3.json("http://localhost:5000/2015_data").then ( data =>{
+            summary_table(data,country_val,year_val);
+            console.log("Year in define function:",year_val);
+    })
+    }
+    if(year_val==2016){
+        d3.json("http://localhost:5000/2016_data").then ( data =>{
+            summary_table(data,country_val,year_val);
+            console.log("Year in define function:",year_val);
+    })
+    }
+    if(year_val==2017){
+        d3.json("http://localhost:5000/2017_data").then ( data =>{
+            summary_table(data,country_val,year_val);
+            console.log("Year in define function:",year_val);
+    })
+    }
+    if(year_val==2018){
+        d3.json("http://localhost:5000/2018_data").then ( data =>{
+            summary_table(data,country_val,year_val);
+            console.log("Year in define function:",year_val);
+    })
+    }
+    if(year_val==2019){
+        d3.json("http://localhost:5000/2019_data").then ( data =>{
+            summary_table(data,country_val,year_val);
+            console.log("Year in define function:",year_val);
+    })
+    }
+
+}
+
+
+function init() {
+    var selectorTwo = d3.select("#selDatasetYear");
+	d3.json("http://localhost:5000/years").then((year_data) => {
+    year_data.forEach((rec) => {
+        year_value=rec.years;
+        // console.log(year_value);
+		selectorTwo
+        .append("option")
+        .text(year_value)
+        .property("value", year_value);
+    });
+    })
+    
+    var selectorThree = d3.select('#selDatasetCountry');
+    d3.json("http://localhost:5000/countries").then((country_data) => {
+    country_data.forEach((rec) => {
+        country_value=rec.country;
+        // console.log(country_value);
+		selectorThree
+        .append("option")
+        .text(country_value)
+        .property("value", country_value);
+
+    });
+    })
+    build_world_map(2019);
+    build_summary_table(2019,"United States");
+   
+}
+
+function optionChangedOne(selectedYear) {
+    // Declaring a global variable to have access to year when country is chosen
+    year_change_value=selectedYear;
+    console.log(selectedYear);
+    d3.select('#myDiv').html("");
+    build_world_map(selectedYear);
+    
+  }
+  
+  function optionChangedTwo(selectedCountry) {
+    ctry_change_value=selectedCountry;
+
+    console.log(selectedCountry);
+    console.log("Year change value",year_change_value)
+    build_summary_table(year_change_value,selectedCountry);
+  }
+
+init();
---
+++ static/js/happiness_summary_table.js
@@ -0,0 +1,80 @@
+function summary_table(data,country_val,year_val)
+{
+    console.log("I am in function_2 for summary table");
+    console.log("year change value passed:",year_val);
+
+    
+        for (i=0;i<data.length;i++){
+            if(country_val==data[i].country){
+                rank=data[i].rank;
+                country=data[i].country;
+                region=data[i].region;
+                score=data[i].score;
+                gdp=data[i].gdp;
+                life_expt=data[i].health_life_expectancy;
+                freedom=data[i].freedom;
+                corruption=data[i].perceptions_of_corruption;
+                generosity=data[i].generosity;
+            }
+        }   
+                
+        var data_array=[['Country','Rank','Score','Region','GDP','Health Life Expectancy','Freedom','Trust Government Corruption','Generosity'],
+                        [country,rank,score,region,gdp,life_expt,freedom,corruption,generosity]];
+    // console.log(generosity);
+    var data = [{
+        type: 'table',
+        columnorder: [1,2],
+        columnwidth: [50,50],
+  
+      header: {
+          values: [["Factors"],["Value"]],
+          align: ["center", "center"],
+          height: 25,
+          line: {
+              width: 1, 
+              color: 'black'
+              },
+          fill: {
+              color: 'black'
+              },
+          font: {
+              family: "'Barlow Condensed' , 'sans-serif'", 
+              size: 17, 
+              color: "white"
+              }
+          },
+        
+      cells: {
+          values: data_array,
+          align: ["left", "center"],
+          height: 25    ,
+          width: 25,
+          line: {
+              color: "black",
+              width: 1
+              },
+          fill: {
+              color: ['white', 'white']
+              },
+          font: {
+              family: "'Barlow Condensed' , 'sans-serif'", 
+              size: 15, 
+              }
+          }
+      }]
+  
+      var layout = {
+          title: `<b> Summary Table - ${country_val}(${year_val})</b>`,
+          width: 500,
+          height: 500,
+          margin: {
+              l: 50,
+              r: 50,
+              b: 100,
+              t: 100,
+          },
+      }
+  
+  
+      Plotly.newPlot('summary_table', data,layout,{responsive: true});	
+}
