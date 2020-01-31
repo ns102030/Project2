2812900c930f8d09b4728334be207650daa6f86e
iraismgarcia5 <iraisgarcia05@gmail.com>
2020-01-31 13:12:35 -0500

    cleaning up branch files

--- config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoibG9saXRhZGlhcyIsImEiOiJjazVlaDFlcWwwMDI4M2VybHI2bGttd3ppIn0.5IueefDOBuG3F8RIAfvJDA";
--- static/js/happiness_guage.js
+++ static/js/happiness_guage.js
@@ -1,121 +1,7 @@
 function guage(score,year_val,country_val)
 {
-<<<<<<< HEAD
-        console.log("I am in function2 for Guage chart");
-        anychart.onDocumentReady(function () 
-        {
-                console.log("Inside anychart function");
-                // For the year 2015
-                if(year_val==2015){
-                        // console.log("if statement for 2015");
-                        anychart.data.loadJsonFile("http://localhost:5000/2015_data",function(data){
-                                console.log("data loaded for 2015");
-                                var score=0;
-                                for(i=0;i<data.length;i++)
-                                {
-                                        if(country_val==data[i].country){
-                                                score=data[i].score;
-                                        }
-                                }
-                        console.log("2015 loop score:",score);
-                        guage_elements(score);
-                        })
-                }
-                // For the year 2016
-                if(year_val==2016){
-                        console.log("if statement for 2016");
-                        anychart.data.loadJsonFile("http://localhost:5000/2016_data",function(data){
-                                // console.log("data loaded for 2016");
-                                var score=0;
-                                for(i=0;i<data.length;i++)
-                                {
-                                        if(country_val==data[i].country){
-                                                score=data[i].score;
-                                        }
-                                }
-                        console.log("2016 loop score:",score);
-                        
-                        guage_elements(score);
-                        })
-                }
-                
-                // For the year 2017
-                if(year_val==2017){
-                        console.log("if statement for 2017");
-                        anychart.data.loadJsonFile("http://localhost:5000/2017_data",function(data){
-                                console.log("data loaded for 2017");
-                                var score=0;
-                                for(i=0;i<data.length;i++)
-                                {
-                                        if(country_val==data[i].country){
-                                                score=data[i].score;
-                                        }
-                                }
-                        console.log("2017 loop score:",score);
-                        
-                        guage_elements(score);
-                        })
-                }
-
-                // For the year 2018
-                if(year_val==2018){
-                        console.log("if statement for 2018");
-                        anychart.data.loadJsonFile("http://localhost:5000/2018_data",function(data){
-                                console.log("data loaded for 2018");
-                                var score=0;
-                                for(i=0;i<data.length;i++)
-                                {
-                                        if(country_val==data[i].country){
-                                                score=data[i].score;
-                                        }
-                                }
-                        console.log("2018 loop score:",score);
-                      
-                        guage_elements(score);
-                        })
-                }
-
-                // For the year 2019
-                if(year_val==2019){
-                        console.log("if statement for 2019");
-                        anychart.data.loadJsonFile("http://localhost:5000/2019_data",function(data){
-                                console.log("data loaded for 2019");
-                                var score=0;
-                                for(i=0;i<data.length;i++)
-                                {
-                                        if(country_val==data[i].country){
-                                                score=data[i].score;
-                                        }
-                                }
-                        console.log("2019 loop score:",score);
-                        
-                        guage_elements(score);
-                        })
-                }
- 
-        })
-        
-}
-     
-function guage_elements(score){
-        // console.log("I am in function2: guage_elements");
-        var gauge = anychart.gauges.circular();
-        gauge.fill('#fff')
-                                        .stroke(null)
-                                        .padding(0)
-                                        .margin(100)
-                                        .startAngle(270)
-                                        .sweepAngle(180);
-        gauge.axis()
-                                        .labels()
-                                        .padding(5)
-                                        .fontSize(14)
-                                        .position('outside')
-                                        .format('{%Value}');
-=======
         
     let degree = parseInt(score) * 20;
->>>>>>> b72fbef132ec9dddc3c23126b06c5189d2296f69
 
 	let level = degree;
 
@@ -184,24 +70,8 @@ function guage_elements(score){
 
 	Plotly.newPlot('guage', trace, layout, {responsive: true});
 
-<<<<<<< HEAD
-        gauge.title()
-                                        .enabled(true)
-                                        .useHtml(true)
-                                        .padding(0)
-                                        .text('Happiness Score<br>' +
-                                                `<span style="color: #212121; font-size: 12px">${score}</span>`) 
-                                        .hAlign('center')   
-                                        .margin([0, 0, 10, 0]);  
-        gauge.container('guage');
-        // console.log("Calling guage draw");
-        // gauge.container(refresh);
-        gauge.draw();
-    
-=======
         
                        
 };
->>>>>>> b72fbef132ec9dddc3c23126b06c5189d2296f69
 
 
